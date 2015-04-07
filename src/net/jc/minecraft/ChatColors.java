95095d31dc5e7b6f71a3b50a30e0d2b4248d5220
Polarcraft <tripps41@hotmail.com>
2015-04-07 14:41:45 -0500

    Create ChatColors.java

---
+++ src/net/jc/minecraft/ChatColors.java
@@ -0,0 +1,20 @@
+package net.jc.minecraft;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class ChatColors implements Listener {
+
+  @EventHandler(priority = EventPriority.NORMAL)
+    public void onPLayerChatEvent(AsyncPlayerChatEvent e) {
+		Player p = e.getPlayer();
+		if (p.hasPermission("yg.chat.color")) { 
+			String message = e.getMessage();
+			message = message.replace('&', '§');
+			e.setMessage(message);
+		}
+	}
+}
